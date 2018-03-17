c0223514cd43e242cfbc836759b45b66e714929f
davidkoojohn <davidkoojohn@gmail.com>
2018-03-17 16:16:54 +0800

    try computed again

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,8 @@
       <h2 class="center">{{ reversedMessage }}</h2>
     </div>
     <todo/>
+    <p>{{ now }}</p>
+    <p>Name: {{ fullName }}</p>
   </div>
 </template>
 
@@ -21,15 +23,27 @@
     },
     data() {
       return {
-        title: 'home-view'
+        title: 'home-view',
+        firstName: 'John',
+        lastName: 'Kuo'
       }
     },
+    // 计算属性
     computed: {
       // 计算属性的 getter
       reversedMessage: function () {
         return this.title.split('').reverse().join('')
+      },
+      now: () => {
+        // 计算属性缓存 vs 方法
+        return Date.now()
+      },
+      fullName() {
+        return `${this.firstName} - ${this.lastName}`
       }
     },
+    // 计算属性 vs 侦听属性
+    watch: {},
     methods: {
 
     }
