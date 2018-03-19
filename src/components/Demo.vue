782b3849bb1a5793a49f3c92a0ededab50156cb8
davidkoojohn <davidkoojohn@gmail.com>
2018-03-19 14:56:10 +0800

    init component

---
+++ src/components/Demo.vue
@@ -0,0 +1,27 @@
+
+<!-- html标签 -->
+<template>
+
+</template>
+
+
+<!-- js脚本，定义组件，实现逻辑 -->
+<script>
+  export default {
+    // name: 组件名称，使用时将把 name 做为标签名 <name></name>
+    name: "demo",
+
+    // props: 组件的属性，用于接收传入参数，在组件中可以使用 this.prop_name 引用
+    // data: 组件初始化数据，定义一个返回对象函数
+    // methods: 方法
+    // created, mounted: 生命周期函数
+    // Vue.use(component) install 方法
+
+  };
+</script>
+
+<!-- 样式 -->
+<style scoped lang="scss">
+  /* scoped 防止样式污染全局 */
+
+</style>
